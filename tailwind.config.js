33273518c0c3a76039debad8d8170d9c0b78c4f6
Harry Potts <hazpotts@gmail.com>
2025-02-10 18:25:49 +0000

    Added custom colour to fix conflict between jetstream and filament

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,20 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'custom': {
+                    '50': '#f5f5f5',
+                    '100': '#f5f5f5',
+                    '200': '#eeeeee',
+                    '300': '#e0e0e0',
+                    '400': '#bdbdbd',
+                    '500': '#9e9e9e',
+                    '600': '#757575',
+                    '700': '#616161',
+                    '800': '#424242',
+                    '900': '#212121',
+                }
+            }
         },
     },
 
